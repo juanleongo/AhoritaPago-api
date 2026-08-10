5463d9970ab960875514c9c8b7b82f8fb420afc4
juan leon <juanjoseleongo@gmail.com>
2026-08-10 17:30:52 -0500

    modificacion del index

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 
-const Server= require('./src/models/server');
+require('dotenv').config();
+const Server = require('./src/models/server');
 
-const server = new Server()
+const server = new Server();
 
 const startServer = async () => {
     try {
-        await server.start()
+        await server.start();
     } catch (error) {
-        console.error('No fue posible iniciar la aplicación:', error.message)
-        process.exitCode = 1
+        console.error('No fue posible iniciar la aplicación:', error.message);
+        process.exitCode = 1;
     }
-}
+};
 
-startServer()
+startServer();
